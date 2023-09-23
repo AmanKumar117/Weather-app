33d2e6ab47a7a694dc6180623e715909de7be944
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-09-24 00:22:41 +0530

    few changes

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -63,7 +63,7 @@ const Weather = ({ weatherdata, temperatureUnit, setTemperatureUnit }) => {
               </div>
             </div>
             <div className="subcont2">
-              <div className="weath">
+              <div className="weath">  {/*when landing on app*/}
                 <p>
                   Temperature: <span>- -</span>
                 </p>
